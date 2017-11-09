d5676cc6e86e21fe7e597fac74344733b0ebd5d7
Sigbjørn Skar <Sigska@Outlook.com>
2017-11-09 15:15:35 +0100

    fixing web

--- CMD/WEB/main.go
+++ CMD/WEB/main.go
@@ -5,11 +5,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-	//"strings"
+	"net/url"
+	"strings"
 	"os"
-//	"encoding/json"
-	"github.com/heroku/Assignment2/CurrencyTicker"
+	"encoding/json"
+//	"github.com/heroku/Assignment2/CurrencyTicker"
 //	"github.com/heroku/Assignment2/CMD/WebHooks"
+	service "github.com/heroku/Assignment2/CurreencyTicker"
 	)
 
 
@@ -52,22 +54,87 @@ func HandlerWebhook(w http.ResponseWriter, r *http.Request) {
 		return
 
 
-
+	
 		}
 	}
 */
 
 
+type SubscriberHandler struct {
+	db 			subscriberDB
+	Monitor 	CurrencyMonitor
+}
+
+func HandleSubRequestPOST(handler *SubscriberHandler)(r http.ResponseWriter, w *http.Request) {
+	var s Subscriber
+	err:= json.NewDecoder(w.Body).Decode(&s)
+
+	if err != nil {
+		fmt.Println("error:  %v", http.StatusBadRequest)
+		return
+	}
+
+	if (!ValidateSub(s)) {
+		fmt.Println("error: %v", http.StatusBadRequest)
+		return
+	}
+	/*
+	// check validity of URL in posted json
+	_, err = url.ParseRequestURI(*s.WebhookURL)
+	if err != nil {
+		respWithCode(&res, http.StatusBadRequest)
+		return
+}	*/
 
 
 
+	id, err2 := handler.db.Add(s)
+	if err2 != nil {
+		fmt.Println("error:   %v", http.StatusInternalServerError)
+	}
+
+	// if all works prints to resposnewriter
+	fmt.Fprint(r, id)
+
+}
+
+
+func HandleRequestPOST(handler *SubscriberHandler)(r http.ResponseWriter, w *http.Request) {
+
+parts := strings.Split(req.URL.String(), "/")
+if len(parts) < 2 {
+	fmt.Println("error:  %v", http.StatusBadRequest )
+	return
+}
+
+
+
+sub, err :=  handler.db.Get(parts[1]) 
+if err != nil {
+	fmt.Println("error:  %v", http.StatusNotFound) 
+	return 
+}
+
+
+
+
+http.Header.Add(res.Header(), "content-type", "application/json")
+
+err := json.Encoder(r,).Encode(sub)
+if err != nil {
+	fmt.Println("error:  %v", http.StatusInternalServerError)
+	return
+}
+
+
+}
 
 
 
 
 func main () {
 
-
+/*
 	Global_db := &CurrencyTicker.CurrencyTickerDB{
 		"mongodb://localhost",
 		"currencyTicker_db",
@@ -75,11 +142,25 @@ func main () {
 	}
 
 	fmt.Println(Global_db.DatabaseURL)
-
+*/
 
 	port := os.Getenv("PORT")
+	fixerIO_url :=service.GetENV("FIXER_IO_URL")
+	mongodb_url := service.GetENV("MONGO_DB_URL")
+	mongoDBDatabaseName := service.GetENV("MONGO_DB_DATABASE_NAME")
+
+	db, err := service.SubscriberMongo
 
 	//port := "localhost:8080"
+
+
+
+
+	// set up handlers
+	http.HandleFunc("/", HandleSubRequest)
+	http.Handlefunc("/latest", HandleLatest)
+	http.HandleFunc("/average", HandleAverage)
+	http.HandleFunc("/triggerall", HandleTriggerAll)
 
 
 	err := http.ListenAndServe(":"+port, nil)
